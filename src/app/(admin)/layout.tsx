6c4c78392310db58a090444dfb39c2b4260a4cf6
nelifzyh <nelifauziyah07@gmail.com>
2025-09-11 23:10:38 +0700

    layout admin

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,21 @@
+import Sidebar from "@/components/Sidebar";
+import TopNavbar from "@/components/TopNavbar";
+
+export default function AdminLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="flex">
+            {/* Sidebar */}
+            <Sidebar />
+
+            {/* Main content */}
+            <div className="ml-64 flex-1 flex flex-col min-h-screen">
+                <TopNavbar />
+                <main className="p-6 bg-gray-100 flex-1">{children}</main>
+            </div>
+        </div>
+    );
+}
